76859924285abbbbbdd245f541a4861c59f14236
guddu lawaniya <guddulawaniya123@gmail.com>
2021-07-19 10:26:21 +0530

    even_and_odd.c

---
+++ even_and_odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    printf("Enter the number ");
+    int no;
+    scanf("%d",&no);
+    if(no%2==0)
+    {
+         printf("even number ");
+    }
+   
+    else
+    printf("odd number ");
+
+}
